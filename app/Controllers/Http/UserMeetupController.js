04bf15096fc52fc2574c7ae73418ef09152e49f7
Thais Moraes <murici.thais@gmail.com>
2019-04-26 11:33:37 -0300

    Adding date filter in next meetups query

--- app/Controllers/Http/UserMeetupController.js
+++ app/Controllers/Http/UserMeetupController.js
@@ -4,7 +4,10 @@ const Meetup = use('App/Models/Meetup')
 
 class UserMeetupController {
   async showNext ({ params }) {
+    const now = new Date()
+
     const nextMeetups = await Meetup.query()
+      .where('date', '>', now)
       .whereDoesntHave('users', usersQuery => {
         usersQuery.wherePivot('user_id', params.id)
       })
