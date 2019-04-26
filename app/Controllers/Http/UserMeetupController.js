e514cc340f9b53499a0ac198087135d75d93bb91
Thais Moraes <murici.thais@gmail.com>
2019-04-26 11:53:45 -0300

    Adding user next recomended meetups listing

--- app/Controllers/Http/UserMeetupController.js
+++ app/Controllers/Http/UserMeetupController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Meetup = use('App/Models/Meetup')
+const User = use('App/Models/User')
 
 class UserMeetupController {
   async showNext ({ params }) {
@@ -19,17 +20,38 @@ class UserMeetupController {
   async showRegistrations ({ params }) {
     const now = new Date()
 
-    const nextMeetups = await Meetup.query()
+    const nextRegistrations = await Meetup.query()
       .where('date', '>', now)
       .whereHas('users', usersQuery => {
         usersQuery.wherePivot('user_id', params.id)
       })
       .fetch()
 
-    return nextMeetups
+    return nextRegistrations
   }
 
-  async showRecomended () {}
+  async showRecomended ({ params }) {
+    const user = await User.query()
+      .where('id', params.id)
+      .with('preferences')
+      .fetch()
+
+    const userPreferences = user.toJSON()[0].preferences
+    const preferenceArray = userPreferences.map(preference => preference.id)
+    const now = new Date()
+
+    const nextRecomendend = Meetup.query()
+      .where('date', '>', now)
+      .whereDoesntHave('users', usersQuery => {
+        usersQuery.wherePivot('user_id', params.id)
+      })
+      .whereHas('themes', usersQuery => {
+        usersQuery.whereInPivot('theme_id', preferenceArray)
+      })
+      .fetch()
+
+    return nextRecomendend
+  }
 }
 
 module.exports = UserMeetupController
