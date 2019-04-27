c8506eee7b35d7f4f3359618e18b58524721935e
Thais Moraes <murici.thais@gmail.com>
2019-04-26 23:48:42 -0300

    Returning user info on session creation

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -1,11 +1,16 @@
 'use strict'
 
+const User = use('App/Models/User')
+
 class SessionController {
-  async store ({ request, auth }) {
+  async store ({ request, auth, response }) {
     const { email, password } = request.all()
 
     const token = await auth.attempt(email, password)
-    return token
+
+    const user = await User.findByOrFail('email', email)
+
+    response.send({ token, user })
   }
 }
 
